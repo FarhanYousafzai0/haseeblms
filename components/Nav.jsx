f2971f0245a98276a0b56511b2547578108bd957
Farhan Yousafzai <coderwithferry@gmail.com>
2025-09-07 17:07:01 +0500

    Logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import { ShimmerButton } from './magicui/shimmer-button'
 import { navitems } from '@/lib/data'
 import { SparklesText } from './magicui/sparkles-text'
+import Image from 'next/image'
 
 const Nav = () => {
   return (
     <div className='flex items-center overflow-hidden relative justify-between w-[95%] mx-auto p-2  rounded-md'>
 
 
-<h1 className='font-semibold text-2xl'>Haseeb</h1>
+
+<div>
+<Image width={50} height={50} src="/Logo.svg" alt=' ' />
+
+
+</div>
 
 
 <div className='md:flex items-center gap-3 hidden '>
